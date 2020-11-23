affc567ed0ea42bbd836ca7d3f7ffdd9d8b0f5f4
hs-nazuna <h.suzuki.lskp@gmail.com>
2020-11-23 12:29:54 +0900

    add base constitution

---
+++ include/sapporo_tdzdd_apps.hpp
@@ -0,0 +1,10 @@
+#ifndef SAPPORO_TDZDD_APPS_HPP
+#define SAPPORO_TDZDD_APPS_HPP
+
+#include "sapporo_apps/ext_operations.hpp"
+#include "tdzdd_apps/graph_data.hpp"
+#include "tdzdd_apps/itemset_data.hpp"
+#include "tdzdd_apps/graph_specs.hpp"
+#include "tdzdd_apps/linear_specs.hpp"
+
+#endif
---
+++ test/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+#include "../include/sapporo_tdzdd_apps.hpp"
+#include "naive_enumeration.hpp"
+
+int main() {
+    cout << "Hello, world!" << endl;
+}
