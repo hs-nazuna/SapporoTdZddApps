44b5d7e0d10fe61a534cf67dee7761ec7d3aca0c
hs-nazuna <h.suzuki.lskp@gmail.com>
2020-11-27 23:43:17 +0900

    update all_apps.hpp

--- include/sapporo_tdzdd_apps/all_apps.hpp
+++ include/sapporo_tdzdd_apps/all_apps.hpp
@@ -7,6 +7,14 @@
 
 namespace sapporo_tdzdd_apps {
 
+tdzdd::DdStructure<2> to_ddstructure(const ZBDD& zbdd) {
+    return tdzdd::DdStructure<2>(tdzdd::SapporoZdd(zbdd));
+}
+
+ZBDD to_zbdd(const tdzdd::DdStructure<2>& dd) {
+    return dd.evaluate(tdzdd::ToZBDD());
+}
+
 } // namespace sapporo_tdzdd_apps
 
 #endif
